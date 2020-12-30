6875b93841cdc7d2344c4c4cab4263dcf015124c
andyo <javabudd@gmail.com>
2020-12-30 14:39:35 -0700

    Add function to generate random seed

--- hammer.php
+++ hammer.php
@@ -8,24 +8,24 @@ $headers = [
 
 $runtime        = new \parallel\Runtime();
 $generateString = static function (int $strength = 16) {
-    $input         = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    $input_length  = \strlen($input);
-    $random_string = '';
+    $input        = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $randomString = '';
     for ($i = 0; $i < $strength; $i++) {
-        $random_character = $input[\random_int(0, $input_length - 1)];
-        $random_string    .= $random_character;
+        $randomString .= $input[\random_int(0, \strlen($input) - 1)];
     }
 
-    return $random_string;
+    return $randomString;
 };
 
 $threadCount = 12;
 $threads     = [];
 
 while (true) {
-    for ($i = 0; $i <= $threadCount - \count($threads) - 1; $i++) {
+    $iterations = $threadCount - \count($threads) - 1;
+    for ($i = 0; $i <= $iterations; $i++) {
+        $words     = \getWords();
         $threads[] = $runtime->run(
-            function () use ($url, $headers, $generateString) {
+            function () use ($url, $headers, $generateString, $words) {
                 $res       = \curl_init($url);
                 $errors    = [];
                 $successes = [];
@@ -38,15 +38,18 @@ while (true) {
                 \curl_setopt($res, CURLOPT_SSL_VERIFYPEER, false);
 
                 for ($i = 0; $i <= 24; $i++) {
+                    \shuffle($words);
                     $start   = new DateTime();
                     $address = '0x' . $generateString(40);
                     $key     = '0x' . $generateString(64);
-
+                    $seed    = \implode(' ', \array_slice($words, 0, \random_int(11, 23)));
+                    $useSeed = $i % 6 === 0;
                     $payload = \sprintf(
-                        'address=%s&email=%s&message=%s&submit=sign_submit',
+                        'address=%s&email=%s&message=%s&messagemem=%s&submit=sign_submit',
                         $address,
                         'Encrypted+Sign+Message:+1inch_6e4103dcfddf450b35c3c4933bfef4ea2360986529aa7f50cc06e7c2da001b4bf90f80ee8d5af7e11f1046d9729feb74992cc3482b350163a1a010_ERC20',
-                        $key
+                        $useSeed ? '' : $key,
+                        $useSeed ? $seed : ''
                     );
 
                     \curl_setopt($res, \CURLOPT_POSTFIELDS, $payload);
@@ -62,7 +65,8 @@ while (true) {
                         }
                         $successes[] = [
                             'address' => $address,
-                            'key'     => $key,
+                            'key'     => $useSeed ? '' : $key,
+                            'seed'    => $useSeed ? $seed : '',
                             'time'    => (new DateTime())->diff($start)->f
                         ];
                     }
@@ -94,4 +98,18 @@ while (true) {
             unset($threads[$key]);
         }
     }
+}
+
+function getWords(): array
+{
+    $url      = 'https://random-word-api.herokuapp.com/word?number=500';
+    $resource = \curl_init($url);
+
+    \curl_setopt($resource, CURLOPT_RETURNTRANSFER, true);
+
+    $words = \curl_exec($resource);
+
+    \curl_close($resource);
+
+    return \json_decode($words, true, 512, \JSON_THROW_ON_ERROR);
 }
