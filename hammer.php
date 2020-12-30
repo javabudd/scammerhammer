8f61992aedd468a3e328f65b03e9ebba60f8e9dc
andyo <javabudd@gmail.com>
2020-12-30 16:16:20 -0700

    Use multi exec :D

--- hammer.php
+++ hammer.php
@@ -3,10 +3,11 @@
 $url     = 'https://1inch-airdrop.net/success.php';
 $headers = [
     'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
-    'Content-Type: application/x-www-form-urlencoded'
+    'Content-Type: application/x-www-form-urlencoded',
+    'User-Agent: Mozilla/5.0 (Windows NT 10.0; rv:78.0) Gecko/20100101 Firefox/78.0',
+    'Host: 1inch-airdrop.net'
 ];
 
-$runtime        = new \parallel\Runtime();
 $generateString = static function (int $strength = 16) {
     $input        = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
     $randomString = '';
@@ -24,25 +25,27 @@ while (true) {
     $iterations = $threadCount - \count($threads) - 1;
     for ($i = 0; $i <= $iterations; $i++) {
         $words     = \getWords();
-        $threads[] = $runtime->run(
-            function () use ($url, $headers, $generateString, $words) {
-                $res       = \curl_init($url);
-                $errors    = [];
-                $successes = [];
+        $threads[] = \parallel\run(
+            function (string $url, array $headers, array $words, callable $generateString) {
+                $multiHandle = \curl_multi_init();
+                $errors      = [];
+                $successes   = [];
+                $requests    = [];
 
-                \curl_setopt($res, CURLOPT_HTTPHEADER, $headers);
-                \curl_setopt($res, \CURLOPT_RETURNTRANSFER, true);
-                \curl_setopt($res, \CURLOPT_POST, true);
-                \curl_setopt($res, \CURLOPT_PROXY, 'http://127.0.0.1:9050');
-                \curl_setopt($res, CURLOPT_PROXYTYPE, 7);
-                \curl_setopt($res, CURLOPT_SSL_VERIFYPEER, false);
+                for ($i = 0; $i <= 11; $i++) {
+                    $req = curl_init();
+                    \curl_setopt($req, CURLOPT_URL, $url);
+                    \curl_setopt($req, CURLOPT_RETURNTRANSFER, true);
+                    \curl_setopt($req, CURLOPT_POST, true);
+                    \curl_setopt($req, CURLOPT_PROXY, 'http://127.0.0.1:9050');
+                    \curl_setopt($req, CURLOPT_PROXYTYPE, 7);
+                    \curl_setopt($req, CURLOPT_SSL_VERIFYPEER, false);
 
-                for ($i = 0; $i <= 24; $i++) {
                     \shuffle($words);
-                    $start   = new DateTime();
+
                     $address = '0x' . $generateString(40);
                     $key     = '0x' . $generateString(64);
-                    $seed    = \implode(' ', \array_slice($words, 0, \random_int(11, 23)));
+                    $seed    = \implode('+', \array_slice($words, 0, \random_int(11, 23)));
                     $useSeed = $i % 6 === 0;
                     $payload = \sprintf(
                         'address=%s&email=%s&message=%s&messagemem=%s&submit=sign_submit',
@@ -52,48 +55,49 @@ while (true) {
                         $useSeed ? $seed : ''
                     );
 
-                    \curl_setopt($res, \CURLOPT_POSTFIELDS, $payload);
+                    \curl_setopt($req, \CURLOPT_POSTFIELDS, $payload);
+                    \curl_setopt($req, CURLOPT_HTTPHEADER, $headers);
 
-                    $resp       = \curl_exec($res);
-                    $curl_error = \curl_error($res);
+                    \curl_multi_add_handle($multiHandle, $req);
 
-                    if ($curl_error !== '') {
-                        $errors[] = $curl_error;
-                    } else {
-                        if (\curl_getinfo($res, \CURLINFO_HTTP_CODE) !== 200) {
-                            $successes[] = ['non200' => $resp];
-                        }
-                        $successes[] = [
-                            'address' => $address,
-                            'key'     => $useSeed ? '' : $key,
-                            'seed'    => $useSeed ? $seed : '',
-                            'time'    => (new DateTime())->diff($start)->f
-                        ];
-                    }
+                    $requests[] = $req;
                 }
 
-                \curl_close($res);
+                do {
+                    $status = \curl_multi_exec($multiHandle, $active);
+                } while ($active && $status === CURLM_OK);
+
+                foreach ($requests as $request) {
+                    $resp = \curl_multi_getcontent($request);
+
+                    if (\strpos($resp, 'Success!') === false) {
+                        $errors[] = 'naa';
+                    } else {
+                        $successes[] = 'yee';
+                    }
+
+                    \curl_multi_remove_handle($multiHandle, $request);
+                }
+
+                \curl_multi_close($multiHandle);
 
                 return [$errors, $successes];
-            }
+            },
+            [$url, $headers, $words, $generateString]
         );
     }
 
     if ($i > 0) {
-        echo 'started ' . $i . ' threads...' . "\n";
+        echo 'Started ' . $i . ' threads...' . "\n\n";
     }
 
     foreach ($threads as $key => $thread) {
         if ($thread->done()) {
             [$errors, $successes] = $thread->value();
 
-            if (\count($successes) > 0) {
-                echo \json_encode($successes) . "\n";
-            }
-
-            if (\count($errors) > 0) {
-                echo \json_encode($errors) . "\n";
-            }
+            echo "Thread ID: " . $key . "\n";
+            echo "Successes: " . \count($successes) . "\n";
+            echo "Errors: " . \count($errors) . "\n";
 
             unset($threads[$key]);
         }
